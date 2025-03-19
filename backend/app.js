86f2eb7346dfb427523a58d4b59fe25e52265dc4
Joshua <amethystguitaristj@gmail.com>
2025-03-19 00:03:10 +0000

    new slight changes

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 import { express } from "express";
+import { BodyParser } from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
